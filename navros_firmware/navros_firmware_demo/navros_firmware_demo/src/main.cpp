033046f828e8140d81b54c8b593dc8ed8494f55c
omom77 <sakharkarom777@gmail.com>
2024-05-12 19:00:05 +0530

    added dc motor encoder and navros remote controller publisher to send values to arduino using the ros framework

--- navros_firmware/navros_firmware_demo/navros_firmware_demo/src/main.cpp
+++ navros_firmware/navros_firmware_demo/navros_firmware_demo/src/main.cpp
@@ -1,116 +1,105 @@
 #include <Arduino.h>
-#include <util/atomic.h> // For the ATOMIC_BLOCK macro
 
-//// Make sure you connect the output pins of the encoder to pins with interrupts 
-//// (for an Arduino Uno that means pins 2 and 3)
-#define ENCA 2 // YELLOW
-#define ENCB 3 // WHITE
-#define PWM 6
-#define IN2 4
-#define IN1 5
+// Steps per 360 Rotation - 345
 
-volatile int posi = 0; // specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
-long prevT = 0;
-float eprev = 0;
-float eintegral = 0;
+#define EncoderPinA 3
+#define EncoderPinB 2
+#define mA1 4
+#define mA2 5
+#define mAEn 6
+#define mB1 7
+#define mB2 8
+#define mBEn 9
+volatile long Encodervalue=0;
+int rps = 320;    // Rotations per Step
+double nRotations;
 
-void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
-  analogWrite(pwm,pwmVal);
-  if(dir == 1){
-    digitalWrite(in1,HIGH);
-    digitalWrite(in2,LOW);
-  }
-  else if(dir == -1){
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-  }
-  else{
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,LOW);
-  }  
+void updateEncoder()
+{
+  if (digitalRead(EncoderPinA)> digitalRead(EncoderPinB))
+    Encodervalue++;
+  else
+    Encodervalue--;
 }
 
-void readEncoder(){
-  int b = digitalRead(ENCB);
-  if(b > 0){
-    posi++;
-  }
-  else{
-    posi--;
-  }
+void forward(int pwma, int pwmb){
+  digitalWrite(mA1,HIGH);
+  digitalWrite(mA2,LOW);
+  analogWrite(mAEn,pwma);
+  digitalWrite(mB1,HIGH);
+  digitalWrite(mB2,LOW);
+  analogWrite(mBEn,pwmb);
+  delay(730);
+  digitalWrite(mA1,LOW);
+  digitalWrite(mA2,LOW);
+  digitalWrite(mB1,LOW);
+  digitalWrite(mB2,LOW);
+  // delay(1000);
+}
+void reverse(int pwma, int pwmb){
+  digitalWrite(mA1,LOW);
+  digitalWrite(mA2,HIGH);
+  analogWrite(mAEn,pwma);
+  digitalWrite(mB1,LOW);
+  digitalWrite(mB2,HIGH);
+  analogWrite(mBEn,pwmb);
+  delay(1000);
+  digitalWrite(mA1,LOW);
+  digitalWrite(mA2,LOW);
+  digitalWrite(mB1,LOW);
+  digitalWrite(mB2,LOW);
+  // delay(1000);
 }
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(ENCA,INPUT);
-  pinMode(ENCB,INPUT);
-  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
-  
-  pinMode(PWM,OUTPUT);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  
-  Serial.println("target pos");
+  pinMode(mA1,OUTPUT);
+  pinMode(mA2,OUTPUT);
+  pinMode(mAEn,OUTPUT);
+  pinMode(mB1,OUTPUT);
+  pinMode(mB2,OUTPUT);
+  pinMode(mBEn,OUTPUT);
+  pinMode(EncoderPinA, INPUT);
+  pinMode(EncoderPinB, INPUT);
+  attachInterrupt(digitalPinToInterrupt(EncoderPinA), updateEncoder, RISING);
+  Serial.begin(115200);
 }
 
 void loop() {
+    // digitalWrite(mA1,HIGH);
+    // digitalWrite(mA2,LOW);
+    // analogWrite(mAEn,255);
+    int x = Serial.readString().toInt();
+    int pwmA = 100;
+    int pwmB = 100;
+    if(x == 1){
+        forward(pwmA, pwmB);
+        // delay(1000);
+        Serial.print("Forward");
+    }
+    else if(x == 2){
+        reverse(pwmA, pwmB);
+        // delay(1000);
+        Serial.print("Reverse");
+    }
+    // else if(x == 3){
+    //     left(pwmB, pwmA);
+    //     // delay(1000);
+    //     Serial.print("Left");
+    // }
+    // else if(x == 4){
+    //     right(pwmB, pwmA);
+    //     // delay(1000);
+    //     Serial.print("Right");
+    // }
+    // forward(pwm, pwm);
+    // reverse();
 
-  // set target position
-  int target = 1200;
-  // int target = 250*sin(prevT/1e6);
+    nRotations = Encodervalue/rps;
 
-  // PID constants
-  float kp = 1;
-  float kd = 0.0;
-  float ki = 0.0;
-
-  // time difference
-  long currT = micros();
-  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
-  prevT = currT;
-
-  // Read the position in an atomic block to avoid a potential
-  // misread if the interrupt coincides with this code running
-  // see: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
-  int pos = 0; 
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    pos = posi;
-  }
-  
-  // error
-  int e = pos - target;
-
-  // derivative
-  float dedt = (e-eprev)/(deltaT);
-
-  // integral
-  eintegral = eintegral + e*deltaT;
-
-  // control signal
-  float u = kp*e + kd*dedt + ki*eintegral;
-
-  // motor power
-  float pwr = fabs(u);
-  if( pwr > 255 ){
-    pwr = 255;
-  }
-
-  // motor direction
-  int dir = 1;
-  if(u<0){
-    dir = -1;
-  }
-
-  // signal the motor
-  setMotor(dir,pwr,PWM,IN1,IN2);
-
-
-  // store previous error
-  eprev = e;
-
-  Serial.print(target);
-  Serial.print(" ");
-  Serial.print(pos);
-  Serial.println();
+    Serial.print("Encoder Raw Values : ");
+    Serial.println(Encodervalue);
+    Serial.print("Rotation Values    : ");
+    Serial.println(nRotations);
+    // delay(100);
+    
 }
-
